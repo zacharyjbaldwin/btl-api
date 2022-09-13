ba4848b0014aa11bb26a9960f2ce5468dd5b719a
zacharyjbaldwin <zacharyjbaldwin@gmail.com>
2022-09-12 21:27:26 -0500

    implemented route for getting all the books

--- controllers/book.controller.js
+++ controllers/book.controller.js
@@ -1,3 +1,5 @@
+const Book = require('../models/book.model');
+
 module.exports.addBook = (req, res) => {
     res.status(501).json({
         message: 'Not implemented.'
@@ -5,7 +7,18 @@ module.exports.addBook = (req, res) => {
 };
 
 module.exports.getAllBooks = (req, res) => {
-    res.status(501).json({
-        message: 'Not implemented.'
-    });
+    Book.find({})
+        .then((books) => {
+            res.status(200).json({
+                message: 'Fetched all books.',
+                count: books.length,
+                books: books
+            });
+        })
+        .catch((error) => {
+            res.status(500).json({
+                message: 'Internal error.',
+                error: error
+            })
+        })
 };
