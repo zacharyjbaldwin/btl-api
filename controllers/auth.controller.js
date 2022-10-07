d87c6f161cfef354227e758054ce7d56f5e6d74c
Erik <its.krei@gmail.com>
2022-10-07 09:11:28 -0500

    fixed requested changes

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -92,7 +92,7 @@ module.exports.signup = (req, res) => {
 
                     user.save()
                         .then((user) => {
-                            Logger.addLog('authentication',`${req.userData.firstname} ${req.userData.lastname} 'created an account'`);
+                            Logger.addLog('authentication',`${req.body.firstname} ${req.body.lastname} created an account`);
                             res.status(201).json({
                                 message: 'User created.',
                                 user: user
