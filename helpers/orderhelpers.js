605b6f57fb7b6e86af704dcb17a344b212d67a19
Erik <52636356+Tarzerk@users.noreply.github.com>
2022-10-16 13:36:18 -0500

    fixed some small stuff

--- helpers/orderhelpers.js
+++
@@ -1,2 +0,0 @@
-// check if the user id is the one associated with the account
-
--- models/order.model.js
+++ models/order.model.js
@@ -1,16 +1,23 @@
 const mongoose = require("mongoose");
 
+const cartSchema = mongoose.Schema({
+    item: { type: mongoose.Schema.Types.ObjectId, ref: 'Book', required: true },
+    quantity: { type: Number, required: true }
+});
+
+const addressSchema = mongoose.Schema({
+    // check figma
+    // address Schema
+});
+
 const orderSchema = mongoose.Schema({
     creator: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    //everything in there
-    contents: { required: true },
-    status: { required: true },
-    address: { type: String, required: true },
-    cartID: { required: true },
-    subTotal: { required: true },
-    total: { required: true },
-    tax: { required: true },
-    shippingPrice: { required: true },
-    timestamp: { type: String, required: true},
+    contents: [cartSchema],
+    // 0 pending, 1 shipped, 2 canceled
+    status: { type: number, required: true },
+    address: addressSchema,
+    subTotal: { type: number, required: true },
+    shippingPrice: { type: number, required: true },
+    timestamp: { type: String, required: true}, 
 });
 module.exports = mongoose.model('Order', orderSchema);
