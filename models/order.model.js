fbc6a6c61aa640cc876fcc49c9bccd2f26471456
Erik <52636356+Tarzerk@users.noreply.github.com>
2022-10-18 08:33:31 -0500

    new edits

--- models/order.model.js
+++ models/order.model.js
@@ -13,8 +13,7 @@ const addressSchema = mongoose.Schema({
 const orderSchema = mongoose.Schema({
     creator: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     contents: [cartSchema],
-    // 0 pending, 1 shipped, 2 canceled
-    status: { type: number, required: true },
+    status: { type: number, required: true },  // 0 pending, 1 shipped, 2 canceled
     address: addressSchema,
     subTotal: { type: number, required: true },
     shippingPrice: { type: number, required: true },
