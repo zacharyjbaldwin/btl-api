3ddd7763e8206681393b075765671a33e84b90e9
Erik <52636356+Tarzerk@users.noreply.github.com>
2022-10-30 10:06:38 -0500

    order stuff

--- controllers/order.controller.js
+++ controllers/order.controller.js
@@ -1,24 +1,32 @@
 const Order = require('../models/order.model');
 
 module.exports.getOrder = (req, res) => {
+    // .populate
     res.status(200).json({
         message: 'order retrived!'
     });
 };
 
 module.exports.addOrder = (req, res) => {
-    
     res.status(200).json({
         message: 'order added!'
     });
 };
 
 module.exports.getAllOrders = (req, res) => {
-    res.status(200).json({
-        message: 'retrived all orders!'
-    });
+    Order.find().populate('contents.BookId').then((order) => {
+        res.status(200).json({
+            message: 'retrived all orders!',
+            order: order,
+        });
+    }).catch(
+        (error)=> {
+            res.status(500).json({
+                error: 'failed to fetch orders', 
+            }); 
+        }
+    )
 };
-
 
 // req.body to check what the thing contains
 module.exports.markShippedOrCanceled = (req, res) => {
--- routes/order.routes.js
+++ routes/order.routes.js
@@ -3,15 +3,19 @@ const mustBeAdmin = require('../middleware/check-admin');
 const router = require('express').Router();
 const orderController = require('../controllers/example.controller');
 
+// POST root/api/orders
 // add order
 router.post('/', orderController.addOrder);
 
+// GET root/api/ordesr/:id
 // get order by id, only admins can get any order otherwise you get order belonging to you
 router.get('/:id', mustBeAuthenticated, orderController.getOrder);
 
-// get all order
-router.get('/', mustBeAuthenticated, mustBeAdmin, orderController.getAllOrders);
+// GET root/api/orders/
+// get all orders
+router.get('/', mustBeAuthenticated, orderController.getAllOrders); // mustBe Authenticated part must be added
 
+// GET root/api/orders/:orderId
 // mark order as shipped or canceled
 router.put('/:orderId', mustBeAuthenticated, orderController.markShippedOrCanceled);
 
